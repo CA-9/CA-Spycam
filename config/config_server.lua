0d1ec08bc407b38f31c8f217150f833961163968
CA.1 <mohammad92007@gmail.com>
2025-08-26 04:18:37 +0400

    Add files via upload

---
+++ config/config_server.lua
@@ -0,0 +1,10 @@
+Config = {}
+
+Config.Debug = false                        -- Enables debug printing to the server console
+
+Config.SpycamItem       = 'spy_camera'          -- Spycam item name
+Config.ControlItem      = 'ca_tablet'   -- Spycam controller item name
+Config.MaxCameras       = 3                 -- Maximum number of deployable spy cameras per player
+
+-- Hook for sending data to Discord
+Config.Hook = ''
